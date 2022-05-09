21a28fa6968de28ab62fcc0eca120d650697a181
“isolcat” <308750414@qq.com>
2022-05-09 17:27:23 +0800

    基于cors解决接口跨域

--- 使用express写接口/1.使用express写接口.js
+++ 使用express写接口/1.使用express写接口.js
@@ -5,6 +5,11 @@ const app = express()
 // 配置解析表单数据的中间件
 app.use(express.urlencoded({ extended: false }))
 
+// 一定要在路由之前配置cors这个中间件，从而解决接口跨域的问题
+// cors主要在服务器端进行配置，客户端浏览器无需进行任何额外的配置
+const cors = require('cors')
+app.use(cors())
+
 // 导入路由模块
 const router = require('./2.apiRouter')
     // 把路由模块注册到app上
