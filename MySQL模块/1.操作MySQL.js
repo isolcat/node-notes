b1e55f44ac3b98a94a062be3ddb99970b730670a
“isolcat” <308750414@qq.com>
2022-05-12 22:46:06 +0800

    查询和插入数据

--- MySQL模块/1.操作MySQL.js
+++ MySQL模块/1.操作MySQL.js
@@ -9,9 +9,47 @@ const db = mysql.createPool({
 })
 
 //测试mysql模块能否正常工作
-db.query('select 1', (err, results) => {
-    // mysql模块工作期间报错了
-    if (err) return console.log(err.message);
-    // 能够成功的执行sql语句
-    console.log(results)
+// db.query('select 1', (err, results) => {
+//     // mysql模块工作期间报错了
+//     if (err) return console.log(err.message);
+//     // 能够成功的执行sql语句
+//     console.log(results)
+// })
+
+
+//查询users表中的所有数据
+// const sqlStr = 'select * from users'
+// db.query(sqlStr, (err, result) => {
+//     // 查询数据失败
+//     if (err) return console.log(err.message);
+//     // 查询数据成功
+//     // 注意：如果执行的是select查询语句，则执行的结果是数组
+//     console.log(result);
+// })
+
+
+// 向users表中，新增一条数据
+// const user = { username: 'Spider-Man', password: 'pcc123' }
+//     // 定义待执行的SQL语句
+// const sqlStr = 'insert into users(username,password) values(?,?)'
+//     // 执行SQL语句
+// db.query(sqlStr, [user.username, user.password], (err, results) => {
+//     if (err) return console.log(err.message)
+//         // 通过affectedRows属性，来判断是否插入数据成功
+//     if (results.affectedRows === 1) {
+//         console.log('插入数据成功！')
+//     }
+// })
+
+
+// 演示插入数据的便捷方式
+const user = { username: 'Spider-Man2', password: 'pcc4321' }
+    // 定义待执行的SQL语句
+const sqlStr = 'insert into users set?'
+    // 执行SQL语句
+db.query(sqlStr, user, (err, result) => {
+    if (err) return console.log(err.message)
+    if (result.affectedRows === 1) {
+        console.log('插入数据成功')
+    }
 })
