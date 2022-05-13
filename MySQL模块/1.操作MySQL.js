f49b734a7e075a117828336881e8ec5f88dba491
“isolcat” <308750414@qq.com>
2022-05-13 19:23:18 +0800

    更新和删除数据

--- MySQL模块/1.操作MySQL.js
+++ MySQL模块/1.操作MySQL.js
@@ -43,13 +43,52 @@ const db = mysql.createPool({
 
 
 // 演示插入数据的便捷方式
-const user = { username: 'Spider-Man2', password: 'pcc4321' }
-    // 定义待执行的SQL语句
-const sqlStr = 'insert into users set?'
-    // 执行SQL语句
-db.query(sqlStr, user, (err, result) => {
-    if (err) return console.log(err.message)
+// const user = { username: 'Spider-Man2', password: 'pcc4321' }
+//     // 定义待执行的SQL语句
+// const sqlStr = 'insert into users set?'
+//     // 执行SQL语句
+// db.query(sqlStr, user, (err, result) => {
+//     if (err) return console.log(err.message)
+//     if (result.affectedRows === 1) {
+//         console.log('插入数据成功')
+//     }
+// })
+
+// 演示如何更新用户的信息
+// const user = { id: 6, username: 'aaa', password: '000' }
+//     // 定义sql语句
+// const sqlStr = 'update users set username=?,password=?where id=?'
+//     // 执行SQL语句
+// db.query(sqlStr, [user.username, user.password, user.id], (err, result) => {
+//     if (err) return console.log(err.message)
+//         // 注意：执行了update语句之后，执行的结果也是一个对象，可以通过affectedRows判断是否更新成功
+//     if (result.affectedRows === 1) {
+//         console.log('更新成功')
+//     }
+// })
+
+
+// 演示更新数据的便捷方式
+// const user = { id: 6, username: 'aaaa', password: '0000' }
+//     // 定义SQL语句
+// const sqlStr = 'update users set ? where id=?'
+//     // 执行SQL语句
+// db.query(sqlStr, [user, user.id], (err, results) => {
+//     if (err) return console.log(err.message);
+//     if (results.affectedRows === 1) {
+//         console.log('更新数据成功');
+//     }
+// })
+
+
+
+// 删除id为5的用户
+const sqlStr = 'delete from users where id=?'
+db.query(sqlStr, 5, (err, result) => {
+    if (err) return console.log(err.message);
     if (result.affectedRows === 1) {
-        console.log('插入数据成功')
+        console.log('删除数据成功');
     }
 })
+
+// 更推荐标记删除，如status(标记状态)来模拟删除
